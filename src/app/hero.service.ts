f4825895020845003118428998ae641032ce39c2
USER <test@test.com>
2018-02-03 17:14:34 +0900

    6. 서비스
    RxJS Objsevable을 이용한 비동기 처리

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HEROES} from './mock-heroes';
 import {Hero} from './hero';
+import {of} from 'rxjs/observable/of';
+import {Observable} from 'rxjs/Observable';
 
 // 1. 등록 : 앵귤러 컨테이너에 등록, 등록시점은 providers가 결정한다.
 // 앵귤러는 클라이언트에서 구동되기 때문에 메모리 제약이 있음.
@@ -10,7 +12,8 @@ export class HeroService {
   constructor() { }
 
   // method(): return Type {...}
-  getHeroes(): Hero[] {
-    return HEROES;
+  // Observable객체 타입이 Hero[]배열 타입이다.
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-heroes',
@@ -23,13 +24,6 @@ export class HeroesComponent implements OnInit {
   // 1. 타입만 선언 후
    hero3: Hero;
   // 2. 생성자에서 만듬 hero.ts에서 생성자 부분으로 가보자
-  /*
-  es5문법은 너무 길어지니까 아래처럼 한줄로 만들자.
-  constructor() {
-    this.hero3 = new Hero();
-    this.hero3.id = 2;
-    this.hero3.name = 'Win Storm';
-  }*/
 
   hero4 = HEROES;   // =hero4: Hero[];
   selectedHero: Hero;
@@ -39,13 +33,22 @@ export class HeroesComponent implements OnInit {
   // 로컬변수니까 public을 써서 다른 기능에도 바로 쓸 수 있도록 한다.
   // 2. 주입 : 생성자로 서비스를 주입한다. (히어로즈를 생성할때 heroService를 바로 주입한다.)
   constructor(public heroService: HeroService) {
+    /* es5문법은 너무 길어지니까 hero3를 아래처럼 한줄로 만들자.
+    this.hero3 = new Hero();
+    this.hero3.id = 2;
+    this.hero3.name = 'Win Storm';*/
     this.hero3 = new Hero(2, 'Win Storm');
     this.hero4 = HEROES;
   }
 
   // 페이지 초기화될 때
   ngOnInit() {
-    this.hero5 = this.heroService.getHeroes();
+    // this.hero5 = this.heroService.getHeroes();
+    // Observable이기 때문에 비동기처리 해야함
+    this.heroService.getHeroes()
+    .subscribe(data => {
+      this.hero5 = data;
+    });
   }
 
   // method
